bda47f5dff1c4c134f2610b401d9d03c9c5e5ce9
Anshulk2004 <anshulk0102@gmail.com>
2024-08-05 18:47:15 +0530

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
 import Image from "next/image";
 import Spline from '@splinetool/react-spline/next';
+import styled from 'styled-components';
+
+const SplineContainer = styled.div`
+  width: 100%;
+  height: 100vh; /* Full viewport height */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    height: 100vh;
+  }
+
+  @media (min-width: 769px) {
+    height: 100vh;
+  }
+`;
 
 export default function Home() {
   return (
     <main>
-      <Spline
-        scene="https://prod.spline.design/UnQM4XpRbf3yvA4s/scene.splinecode" 
-      />
+      <SplineContainer>
+        <Spline
+          scene="https://prod.spline.design/UnQM4XpRbf3yvA4s/scene.splinecode" 
+        />
+      </SplineContainer>
     </main>
   );
 }
-      
-          
-
-
